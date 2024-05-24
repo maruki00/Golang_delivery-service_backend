498a093e44f8b3cfae6bf791506c89832c892a44
Abdellah oulahyane <aitbaha48@gmail.com>
2024-05-24 15:54:58 +0300

    --

--- main.sql
+++ main.sql
@@ -9,50 +9,98 @@ create table users(
     email varchar(255) not null unique,
     address text not null,
     password text not null,
-    type varchar(56) not null default 'costumer'
+    type varchar(56) not null default 'costumer',    
+    created_at timestamp default now(),
+    updated_at timestamp default now(),
 );
 
 create table auth(
     id int primary key primary key auto_increment,
     email varchar(255) not null unique,
     token text not null unique,
-    user_id int not null
+    user_id int not null,
+    user_type varchar(56) not null,
+    user_level varchar(56) not null,
+    created_at timestamp default now(),
+    updated_at timestamp default now(),
 );
 
 create table files (
-    int primary key not null auto_increment
+    int primary key not null auto_increment,
+    path varchar(255) not null,
+    hash varchar(100) not null,
+    extention varchar(10) not null,
+    created_at timestamp default now(),
+    updated_at timestamp default now(),
 );
 
 create table orders(
     id int primary key not null auto_increment,
 
+
+
+    created_at timestamp default now(),
+    updated_at timestamp default now()
 );
 
 create table users_orders(
     id long primary key not null auto_increment,
-    
+
+
+
+    created_at timestamp default now(),
+    updated_at timestamp default now()
+
 );
 
 create table products(
     id int primary key not null auto_increment,
+
+
+
+    created_at timestamp default now(),
+    updated_at timestamp default now()
 );
 
 create table feedbacks(
     id int primary key not null auto_increment,
+
+
+
+    created_at timestamp default now(),
+    updated_at timestamp default now()
 );
 
 create table analytics(
     id int primary key not null auto_increment,
+
+
+    created_at timestamp default now(),
+    updated_at timestamp default now()
 );
 
 create table orders_tracking(
     id int primary key not null auto_increment,
+
+
+
+    created_at timestamp default now(),
+    updated_at timestamp default now()
 );
 
 create table notifications(
     id int primary key not null auto_increment,
+
+
+    created_at timestamp default now(),
+    updated_at timestamp default now()
 );
 
 create table jobs(
     id int primary key not null auto_increment,
+
+
+
+    created_at timestamp default now(),
+    updated_at timestamp default now()
 );
