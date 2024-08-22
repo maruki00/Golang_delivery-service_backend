a972fcaba0fd5e8e313853e7d495ed1460752dc1
maruki00 <aitbaha48@gmail.com>
2024-08-22 19:45:30 +0300

    --

--- Services/Shared/Infrastructure/Core/Validator.go
+++ Services/Shared/Infrastructure/Core/Validator.go
@@ -1,14 +1,25 @@
 package shared_core
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator"
 )
 
 type RequestValidator struct {
-	validator *validator.Validate
+	Validator *validator.Validate
 }
 
-func (o *RequestValidator) Validate(ctx gin.Context, request *interface{}) {
+func (o *RequestValidator) Validate(ctx gin.Context, request interface{}) (error, interface{}) {
 
+	if err := ctx.BindJSON(&request); err != nil {
+		return err, nil
+	}
+
+	if err := o.Validator.Struct(request); err != nil {
+		validationErrors := err.(validator.ValidationErrors)
+		return fmt.Errorf("Validation failed for field: %s", validationErrors[0].Field()), nil
+	}
+	return nil, request
 }
