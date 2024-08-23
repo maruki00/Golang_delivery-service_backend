585b15b1e642683040848133cda7909a00e831de
maruki00 <aitbaha48@gmail.com>
2024-08-23 15:02:01 +0300

    --

--- Services/Product/Infrastructure/Repositories/Mysql/ProductRepsoitory.go
+++ Services/Product/Infrastructure/Repositories/Mysql/ProductRepsoitory.go
@@ -1,124 +1,112 @@
-// package mysql_product_repository
+package mysql_product_repository
 
-// import (
-// 	"crypto/md5"
+import (
+	"crypto/md5"
 
-// 	shareddb "delivery/Services/Shared/Infrastructure/DB"
-// 	"errors"
-// 	"fmt"
-// )
+	product_domain_entities "delivery/Services/Product/Domian/Entities"
+	shareddb "delivery/Services/Shared/Infrastructure/DB"
+	"errors"
+	"fmt"
 
-// type ProductRepository struct {
-// }
+	"gorm.io/gorm"
+)
 
-// func NewProductRepository() *ProductRepository {
-// 	return &ProductRepository{}
-// }
+type ProductRepository struct {
+	db *gorm.DB
+}
 
-// func (obj *ProductRepository) Insert(label string, price PriceVauleObject) error {
-// 	db := shareddb.NewDB()
-// 	defer db.Close()
-// 	dto := &DTOs.LoginDTO{}
+func NewProductRepository(dbObj *gorm.DB) *ProductRepository {
+	return &ProductRepository{
+		db: dbObj,
+	}
+}
 
-// 	hash := md5.Sum([]byte(password))
+func (obj *ProductRepository) Insert(product product_domain_entities.ProductEntity) error {
+	db := shareddb.NewDB()
+	defer db.Close()
+	dto := &DTOs.LoginDTO{}
 
-// 	h := fmt.Sprintf("%x", hash)
+	hash := md5.Sum([]byte(password))
 
-// 	statement, _ := db.Prepare("SELECT email, password FROM users WHERE email = ? and password = ? limit 1")
-// 	defer statement.Close()
-// 	statement.QueryRow(login, h).Scan(&dto.Login, &dto.Password)
-// 	if dto.Failed() {
-// 		return errors.New("invalid credential")
-// 	}
-// 	return nil
-// }
+	h := fmt.Sprintf("%x", hash)
 
-// func (obj *ProductRepository) Insert(login string, password string) error {
-// 	db := shareddb.NewDB()
-// 	defer db.Close()
-// 	dto := &DTOs.LoginDTO{}
+	statement, _ := db.Prepare("SELECT email, password FROM users WHERE email = ? and password = ? limit 1")
+	defer statement.Close()
+	statement.QueryRow(login, h).Scan(&dto.Login, &dto.Password)
+	if dto.Failed() {
+		return errors.New("invalid credential")
+	}
+	return nil
+}
 
-// 	hash := md5.Sum([]byte(password))
+func (obj *ProductRepository) Update(login string, password string) error {
+	db := shareddb.NewDB()
+	defer db.Close()
+	dto := &DTOs.LoginDTO{}
 
-// 	h := fmt.Sprintf("%x", hash)
+	hash := md5.Sum([]byte(password))
 
-// 	statement, _ := db.Prepare("SELECT email, password FROM users WHERE email = ? and password = ? limit 1")
-// 	defer statement.Close()
-// 	statement.QueryRow(login, h).Scan(&dto.Login, &dto.Password)
-// 	if dto.Failed() {
-// 		return errors.New("invalid credential")
-// 	}
-// 	return nil
-// }
+	h := fmt.Sprintf("%x", hash)
 
-// func (obj *ProductRepository) Update(login string, password string) error {
-// 	db := shareddb.NewDB()
-// 	defer db.Close()
-// 	dto := &DTOs.LoginDTO{}
+	statement, _ := db.Prepare("SELECT email, password FROM users WHERE email = ? and password = ? limit 1")
+	defer statement.Close()
+	statement.QueryRow(login, h).Scan(&dto.Login, &dto.Password)
+	if dto.Failed() {
+		return errors.New("invalid credential")
+	}
+	return nil
+}
 
-// 	hash := md5.Sum([]byte(password))
+func (obj *ProductRepository) Delete(login string, password string) error {
+	db := shareddb.NewDB()
+	defer db.Close()
+	dto := &DTOs.LoginDTO{}
 
-// 	h := fmt.Sprintf("%x", hash)
+	hash := md5.Sum([]byte(password))
 
-// 	statement, _ := db.Prepare("SELECT email, password FROM users WHERE email = ? and password = ? limit 1")
-// 	defer statement.Close()
-// 	statement.QueryRow(login, h).Scan(&dto.Login, &dto.Password)
-// 	if dto.Failed() {
-// 		return errors.New("invalid credential")
-// 	}
-// 	return nil
-// }
+	h := fmt.Sprintf("%x", hash)
 
-// func (obj *ProductRepository) Delete(login string, password string) error {
-// 	db := shareddb.NewDB()
-// 	defer db.Close()
-// 	dto := &DTOs.LoginDTO{}
+	statement, _ := db.Prepare("SELECT email, password FROM users WHERE email = ? and password = ? limit 1")
+	defer statement.Close()
+	statement.QueryRow(login, h).Scan(&dto.Login, &dto.Password)
+	if dto.Failed() {
+		return errors.New("invalid credential")
+	}
+	return nil
+}
 
-// 	hash := md5.Sum([]byte(password))
+func (obj *ProductRepository) GetById(login string, password string) error {
+	db := shareddb.NewDB()
+	defer db.Close()
+	dto := &DTOs.LoginDTO{}
 
-// 	h := fmt.Sprintf("%x", hash)
+	hash := md5.Sum([]byte(password))
 
-// 	statement, _ := db.Prepare("SELECT email, password FROM users WHERE email = ? and password = ? limit 1")
-// 	defer statement.Close()
-// 	statement.QueryRow(login, h).Scan(&dto.Login, &dto.Password)
-// 	if dto.Failed() {
-// 		return errors.New("invalid credential")
-// 	}
-// 	return nil
-// }
+	h := fmt.Sprintf("%x", hash)
 
-// func (obj *ProductRepository) GetById(login string, password string) error {
-// 	db := shareddb.NewDB()
-// 	defer db.Close()
-// 	dto := &DTOs.LoginDTO{}
+	statement, _ := db.Prepare("SELECT email, password FROM users WHERE email = ? and password = ? limit 1")
+	defer statement.Close()
+	statement.QueryRow(login, h).Scan(&dto.Login, &dto.Password)
+	if dto.Failed() {
+		return errors.New("invalid credential")
+	}
+	return nil
+}
 
-// 	hash := md5.Sum([]byte(password))
+func (obj *ProductRepository) Search(login string, password string) error {
+	db := shareddb.NewDB()
+	defer db.Close()
+	dto := &DTOs.LoginDTO{}
 
-// 	h := fmt.Sprintf("%x", hash)
+	hash := md5.Sum([]byte(password))
 
-// 	statement, _ := db.Prepare("SELECT email, password FROM users WHERE email = ? and password = ? limit 1")
-// 	defer statement.Close()
-// 	statement.QueryRow(login, h).Scan(&dto.Login, &dto.Password)
-// 	if dto.Failed() {
-// 		return errors.New("invalid credential")
-// 	}
-// 	return nil
-// }
+	h := fmt.Sprintf("%x", hash)
 
-// func (obj *ProductRepository) Search(login string, password string) error {
-// 	db := shareddb.NewDB()
-// 	defer db.Close()
-// 	dto := &DTOs.LoginDTO{}
-
-// 	hash := md5.Sum([]byte(password))
-
-// 	h := fmt.Sprintf("%x", hash)
-
-// 	statement, _ := db.Prepare("SELECT email, password FROM users WHERE email = ? and password = ? limit 1")
-// 	defer statement.Close()
-// 	statement.QueryRow(login, h).Scan(&dto.Login, &dto.Password)
-// 	if dto.Failed() {
-// 		return errors.New("invalid credential")
-// 	}
-// 	return nil
-// }
+	statement, _ := db.Prepare("SELECT email, password FROM users WHERE email = ? and password = ? limit 1")
+	defer statement.Close()
+	statement.QueryRow(login, h).Scan(&dto.Login, &dto.Password)
+	if dto.Failed() {
+		return errors.New("invalid credential")
+	}
+	return nil
+}
