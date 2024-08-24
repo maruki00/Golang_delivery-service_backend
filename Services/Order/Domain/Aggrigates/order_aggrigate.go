6758f526e479fd8f23c7691c8e1460db58a388d5
maruki00 <aitbaha48@gmail.com>
2024-08-25 01:21:08 +0300

    --

--- Services/Order/Domain/Aggrigates/order_aggrigate.go
+++ Services/Order/Domain/Aggrigates/order_aggrigate.go
@@ -1,7 +1,11 @@
 package orderaggrigate
 
-import order_entities "delivery/Services/Order/Domain/Entities"
+import (
+	order_entities "delivery/Services/Order/Domain/Entities"
+	product_domain_entities "delivery/Services/Product/Domian/Entities"
+)
 
 type OrderAggrigate struct {
 	orderEntity order_entities.OrderEntity
+	items       []product_domain_entities.ProductEntity
 }
