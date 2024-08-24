ead605103cdd2af4f1d78c45982915144810c68e
maruki00 <aitbaha48@gmail.com>
2024-08-24 15:04:07 +0300

    --

--- Services/Product/UserGetway/Controllers/ProductController.go
+++ Services/Product/UserGetway/Controllers/ProductController.go
@@ -1,8 +1,17 @@
 package product_usergetway_controllers
 
+import (
+	product_services "delivery/Services/Product/Application/Services"
+	product_Infrastructure_repository "delivery/Services/Product/Infrastructure/Repositories"
+)
+
 type ProductController struct {
+	service product_services.ProductService
 }
 
 func NewProductController() *ProductController {
-
+	repo := product_Infrastructure_repository.NewProductRepository()
+	return &ProductController{
+		service: &product_services.NewProductService(),
+	}
 }
