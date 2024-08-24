7ffa466d8c9f962ae3c75b526f2c76a2353040dc
maruki00 <aitbaha48@gmail.com>
2024-08-24 15:25:27 +0300

    --

--- Services/Product/Application/Services/ProductService.go
+++ Services/Product/Application/Services/ProductService.go
@@ -1,24 +1,15 @@
 package product_services
 
 import (
-	product_domain_dtos "delivery/Services/Product/Domian/DTOS"
 	product_domain_repositories "delivery/Services/Product/Domian/Repositories"
 )
 
 type ProductService struct {
 	productRepository *product_domain_repositories.IProductRepository
-	// outputPort        *product_domain_ports.ProductOutputPort
 }
 
-func NewProductService(productRepository product_domain_repositories.IProductRepository,
-	//outputPort product_domain_ports.NewProductOutputPort,
-	model interface{}) *ProductService {
+func NewProductService(productRepository product_domain_repositories.IProductRepository) *ProductService {
 	return &ProductService{
 		productRepository: &productRepository,
-		// outputPort:        &outputPort,
 	}
-}
-
-func (this ProductService) AddProduct(dto product_domain_dtos.ProductDTO) (error, ViewModel) {
-	this.outputPortl.
 }
