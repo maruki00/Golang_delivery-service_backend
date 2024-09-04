3bdbcfe264eaa9e80d83226b2a46172e8f1e076e
maruki00 <aitbaha48@gmail.com>
2024-09-04 23:52:05 +0300

    --

--- Services/Order/Infrastructure/Models/Order.go
+++ Services/Order/Infrastructure/Models/Order.go
@@ -1,16 +1,31 @@
 package order_infrastructues_models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type OrderModel struct {
 	gorm.Model
 	Id               int     `json:"id"`
 	OrderFingerprint string  `json:"order_fingerprint"`
 	CostumerId       int     `json:"costumer_id"`
-	FromLong         float32 `json:"from_long"`
-	FromLat          float32 `json:"from_lat"`
-	ToLong           float32 `json:"to_long"`
-	ToLat            float32 `json:"to_lat"`
-	// CreatedAt        `json:"created_at"`
-	// UpdatedAt        timestamp `json:"updated_at"`
+	TotalPrice       float32 `json: "total_price"`
+	// FromLong         float32 `json:"from_long"`
+	// FromLat          float32 `json:"from_lat"`
+	// ToLong float32 `json:"to_long"`
+	// ToLat  float32 `json:"to_lat"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func (obj *OrderModel) GetId() int {
+	return obj.Id
+}
+func (obj *OrderModel) GetOrderFingerprint() string {
+	return obj.OrderFingerprint
+}
+func (obj *OrderModel) GetCostumerId() int {
+	return obj.CostumerId
 }
