464a2f06747399a2b65aacfcb825f054efab68fc
maruki00 <aitbaha48@gmail.com>
2024-09-05 22:48:18 +0300

    --

--- Services/Product/Domian/Ports/NewProductInputPort.go
+++ Services/Product/Domian/Ports/NewProductInputPort.go
@@ -1,10 +1,10 @@
 package product_domain_ports
 
-import (
-	product_domain_dtos "delivery/Services/Product/Domian/DTOS"
-	adapters_viewmodels "delivery/app/adapters/ViewModels"
-)
+// import (
+// 	product_domain_dtos "delivery/Services/Product/Domian/DTOS"
+// 	adapters_viewmodels "delivery/app/adapters/ViewModels"
+// )
 
-type NewProductOutputPort interface {
-	handel(requestmodel product_domain_dtos.ProductDTO) adapters_viewmodels.JsonViewModel
-}
+// type NewProductOutputPort interface {
+// 	handel(requestmodel product_domain_dtos.ProductDTO) adapters_viewmodels.JsonViewModel
+// }
