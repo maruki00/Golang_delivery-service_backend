772d827749894a87b3b789667a6ed44804519d9d
maruki00 <aitbaha48@gmail.com>
2024-09-08 07:17:53 +0300

    --

--- Services/Order/Application/Routes/orders.go
+++ Services/Order/Application/Routes/orders.go
@@ -1,16 +1,14 @@
 package routes
 
 import (
-	Authcontrollers "delivery/Services/Auth/UserGateway/Controllers"
-
 	"github.com/gin-gonic/gin"
 )
 
 var OrderRouter = func(router *gin.Engine) {
 	order := router.Group("/order/")
-	_ = order.POST("/create", Authcontrollers.Register)
-	_ = order.POST("/cancel", Authcontrollers.Register)
-	_ = order.POST("/history/{id}", Authcontrollers.Register)
-	_ = order.POST("/pickup-confirm/{id}", Authcontrollers.Register)
-	_ = router.POST("/chat", Authcontrollers.Register)
+	_ = order.POST("/create", nil)
+	_ = order.POST("/cancel", nil)
+	_ = order.POST("/history/{id}", nil)
+	_ = order.POST("/pickup-confirm/{id}", nil)
+	_ = router.POST("/chat", nil)
 }
