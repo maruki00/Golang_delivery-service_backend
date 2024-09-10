48766e34a2fae40e578f598405995ebc0087fa35
maruki00 <aitbaha48@gmail.com>
2024-09-11 02:46:22 +0300

    --

--- Services/Auth/Domain/Contracts/IAuthRepository.go
+++ Services/Auth/Domain/Contracts/IAuthRepository.go
@@ -1,7 +1,22 @@
 package domain_auth_contracts
 
-import auth_domain_dtos "delivery/Services/Auth/Domain/DTOs"
+import (
+	auth_domain_entities "delivery/Services/Auth/Domain/Entities"
+	auth_infrastructure_models "delivery/Services/Auth/Infrastructure/Models"
+	shared_entities "delivery/Services/Shared/Domain/Entities"
+	shared_models "delivery/Services/Shared/Infrastructure/Models"
+)
 
 type IAuthRepository interface {
-	Login(login, password string) (*auth_domain_dtos.AuthDTO, error)
+	// Login(login, password string) (*auth_domain_dtos.AuthDTO, error)
+	CheckToken(token string) bool
+	Login(login, password string) (*shared_models.User, error)
+	CreateAuth(token string, user *shared_models.User) *auth_infrastructure_models.Auth
+	ClearToken(id int)
+	Register(user shared_entities.UserEntity) (shared_entities.UserEntity, error)
+	CleanPins(email string)
+	TwoFactoryCreate(twofactory auth_domain_entities.TwoFactoryPinEntity) (bool, error)
+	TwoFactoryConfirm(email string, pin int) (bool, error)
+	LockUser(email string, lock string)
+	Logout(token string)
 }
