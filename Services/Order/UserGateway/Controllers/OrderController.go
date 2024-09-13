ab391a36542bb6700d53dcf9ddbc9d8e2bff48f1
maruki00 <aitbaha48@gmail.com>
2024-09-13 21:16:36 +0300

    ==

--- Services/Order/UserGateway/Controllers/OrderController.go
+++ Services/Order/UserGateway/Controllers/OrderController.go
@@ -2,6 +2,11 @@ package order_usergateway_controllers
 
 import (
 	order_domain_ports "delivery/Services/Order/Domain/Ports"
+	order_usergateway_requests "delivery/Services/Order/UserGateway/Requests"
+	shared_models "delivery/Services/Shared/Infrastructure/Models"
+	"fmt"
+	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator"
@@ -14,15 +19,37 @@ type OrderController struct {
 	Validate  *validator.Validate
 }
 
-func NewOrderController(context context.Context, inputPort order_domain_ports.OrderInputPort) *OrderController {
+func NewOrderController(inputPort order_domain_ports.OrderInputPort) *OrderController {
+	ctx, cancleFunc := context.WithTimeout(context.Background(), time.Second*1)
+	defer cancleFunc()
 	return &OrderController{
-		context:   context,
+		context:   ctx,
 		inputPort: inputPort,
 		Validate:  validator.New(),
 	}
 }
 
+func (obj *OrderController) Create(ctx *gin.Context) {
+	request := &order_usergateway_requests.CreateOrderRequest{}
+	if err := ctx.BindJSON(request); err != nil {
+		ctx.JSON(http.StatusBadRequest, &shared_models.ResponseModel{
+			Status:  http.StatusBadRequest,
+			Error:   "invalid request params",
+			Result:  nil,
+			Message: "request error",
+		})
+		return
+	}
+	if err := obj.Validate.Struct(request); err != nil {
+		validationErrors := err.(validator.ValidationErrors)
+		errorMessage := fmt.Sprintf("Validation failed for field: %s", validationErrors[0].Field())
+		ctx.JSON(http.StatusBadRequest, &shared_models.ResponseModel{
+			Status:  http.StatusBadRequest,
+			Error:   errorMessage,
+			Result:  nil,
+			Message: "request error",
+		})
+		return
+	}
 
-func (obj*OrderController) Create(ctx *gin.Context) {
-	var 
 }
--- Services/Order/UserGateway/Requests/CreateOrderRequest.go
+++ Services/Order/UserGateway/Requests/CreateOrderRequest.go
@@ -1,6 +1,11 @@
 package order_usergateway_requests
 
+type OrderProduct struct {
+	ProductId int `json:"product_id"`
+	Qty       int `json:"qty"`
+}
+
 type CreateOrderRequest struct {
-	CostomerId int
-	Products   [][2]int
+	CostomerId    int            `json:"costomer_id"`
+	OrderProducts []OrderProduct `json:"order_products"`
 }
