d0ff441224093dab18af70501345e34d0dd359c5
maruki00 <aitbaha48@gmail.com>
2024-11-07 22:39:24 +0300

    --

--- pkg/rabbitmq/rabbitmq.go
+++ pkg/rabbitmq/rabbitmq.go
@@ -1,1 +1,30 @@
 package pkg_rabbitmq
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+	"github.com/streadway/amqp"
+)
+
+type RabbitMQ struct {
+	Conn      *amqp.Connection
+	Channel   *amqp.Channel
+	QueneName string
+	// RabbitMQDSN string
+}
+
+func NewRAbbitMQ(rabbitMQDSN string, queneName string) *RabbitMQ {
+	conn, err := amqp.Connection(rabbitMQDSN)
+	if err != nil {
+		panic(err)
+	}
+	ch, err := conn.Channel()
+	if err != nil {
+		panic(err)
+	}
+
+	return &RabbitMQ{
+		Conn:      conn,
+		Channel:   ch,
+		QueneName: queneName,
+	}
+}
