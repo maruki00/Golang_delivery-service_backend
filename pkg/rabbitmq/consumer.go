d5d7423e8940de4eeda274cb2d4a770b49fb8cc0
maruki00 <aitbaha48@gmail.com>
2025-02-16 18:20:54 +0100

    commit

--- pkg/rabbitmq/consumer.go
+++ pkg/rabbitmq/consumer.go
@@ -1,4 +1,64 @@
 package pkg_rabbitmq
 
+import (
+	"context"
 
-func Consumer()
+	"github.com/rabbitmq/amqp091-go"
+)
+
+type Consumer struct {
+	AMQPConn *amqp091.Connection
+	Queues   []string
+}
+
+func NewConsumer(dsn string, queues []string) (*Consumer, error) {
+	conn, err := amqp091.Dial(dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Consumer{
+		AMQPConn: conn,
+		Queues:   queues,
+	}, nil
+}
+
+func (c *Consumer) Start(worder func(ctx context.Context, delivery <-chan amqp091.Delivery)) {
+	ch, err := c.AMQPConn.Channel()
+	if err != nil {
+		panic("error: could not oppen the channel")
+	}
+	if err := ch.Qos(1, 1, false); err != nil {
+		panic("error: error Qos")
+	}
+	for _, queueName := range c.Queues {
+		queue, err := ch.QueueDeclare(
+			queueName,
+			true,
+			false,
+			false,
+			false,
+			nil,
+		)
+		if err != nil {
+			panic(err)
+		}
+		deliveryMessages, err := ch.Consume(
+			queue.Name,
+			"",
+			false,
+			false,
+			false,
+			false,
+			nil,
+		)
+		if err != nil {
+			panic(err)
+		}
+		ctx := context.Background()
+		for range 10 {
+			go worder(ctx, deliveryMessages)
+		}
+
+	}
+}
