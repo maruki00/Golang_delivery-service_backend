653616103805d0ce3020a97da12cadeaabf9fb6f
maruki00 <aitbaha48@gmail.com>
2025-02-18 05:51:26 +0100

    commit

--- pkg/postgres/poswtgres.go
+++ pkg/postgres/poswtgres.go
@@ -1,1 +1,34 @@
 package pkg_postgres
+
+import (
+	"database/sql"
+	"errors"
+	"log/slog"
+)
+
+const (
+	MAX_TRIES = 3
+)
+
+type PGHandler struct {
+	db *sql.DB
+}
+
+func NewPGHandler(dns string) (*PGHandler, func(), error) {
+	counter := MAX_TRIES
+	for counter > 0 {
+		slog.Info("try to connect to db")
+		db, err := sql.Open("postgres", dns)
+		if err != nil {
+			continue
+		}
+		return &PGHandler{
+			db: db,
+		}, func() { _ = db.Close() }, nil
+	}
+	return nil, nil, errors.New("could not connect to db")
+}
+
+func (pg *PGHandler) GetDB() *sql.DB {
+	return pg.db
+}
