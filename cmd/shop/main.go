66f0e03b6475b14a660fc5ed032a74197d91501b
maruki00 <aitbaha48@gmail.com>
2025-02-24 22:16:26 +0000

    commit

--- cmd/shop/main.go
+++ cmd/shop/main.go
@@ -1,24 +1,14 @@
 package main
 
 import (
-	auth_routes "delivery/internal/auth/Application/Routes"
-	order_routes "delivery/internal/order/Application/Routes"
-	prouduct_routes "delivery/internal/product/Application/Routes"
-	shared_configs "delivery/internal/shared/Application/Configs"
-	shareddb "delivery/internal/shared/infra/DB"
-
+	"delivery/cmd/shop/configs"
 	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	config, _ := shared_configs.GetConfig()
-	db := shareddb.NewMysqlDB_GORM(config)
+	config, _ := configs.GetConfig(".")
 	router := gin.Default()
-	go auth_routes.AuthRouter(router, db)
-	prouduct_routes.ProductRouter(router, db)
-	order_routes.OrderRouter(router, db)
-
-	router.Run(fmt.Sprintf("%s:%s", config.Server.Host, config.Server.Port))
+	router.Run(fmt.Sprintf("%s:%s", config.RestServer.Host, config.RestServer.Port))
 }
